1f3384ff6052551bdf40e5c43530073101ff64ba
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-07-13 20:57:39 +0200

    trying conversions

--- src/main/scala/fs2chat/conversion/CE.scala
+++ src/main/scala/fs2chat/conversion/CE.scala
@@ -1,13 +1,57 @@
 package fs2chat.conversion
 
+import cats.effect
+import cats.effect.kernel
+import cats.effect.kernel.{CancelScope, Sync => SyncC}
 import kyo.*
+import kyo.kernel.ArrowEffect
 
-opaque type CE[C[F[_]]] <: Env[C[CE.F]] = Env[C[CE.F]]
+import scala.concurrent.duration.FiniteDuration
+
+sealed trait CE[C[F[_]]] extends ArrowEffect[Const[Unit], Const[C[CE.F]]]
 
 object CE:
   opaque type F[X] = X < Any
 
   class ToKyo[C[X[_]]]:
-    def apply[A](body: C[F] => F[A])(using Tag[C[F]]): A < CE[C] = Env.use[C[F]](body)
+    def apply[A](body: C[F] ?=> F[A])(using Tag[CE[C]]): A < CE[C] =
+      ArrowEffect.suspendWith[Any](Tag[CE[C]], ())(env => body(using env))
 
   def apply[C[X[_]]]: ToKyo[C] = new ToKyo[C]
+
+  private object KyoSync extends effect.Sync[KyoSync.F] {
+    type F[A] = A < Sync
+
+    override def suspend[A](hint: SyncC.Type)(thunk: => A): F[A] = Sync.defer(thunk)
+
+    override def monotonic: F[FiniteDuration] = ???
+
+    override def realTime: F[FiniteDuration] = ???
+
+    override def rootCancelScope: CancelScope = ???
+
+    override def forceR[A, B](fa: F[A])(fb: F[B]): F[B] = ???
+
+    override def uncancelable[A](body: kernel.Poll[F] => F[A]): F[A] = ???
+
+    override def canceled: F[Unit] = ???
+
+    override def onCancel[A](fa: F[A], fin: F[Unit]): F[A] = ???
+
+    override def raiseError[A](e: Throwable): F[A] = ???
+
+    override def handleErrorWith[A](fa: F[A])(f: Throwable => F[A]): F[A] =
+      fa.unpanic.recover(f)
+
+    override def pure[A](x: A): F[A] = x
+
+    override def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] = fa.map(f)
+
+    override def tailRecM[A, B](a: A)(f: A => F[Either[A, B]]): F[B] = ???
+  }
+
+  def run[A, S](v: A < (S & CE[effect.Sync])): A < (S & Sync) =
+
+    ArrowEffect.handle(Tag[CE[effect.Sync]], v)(
+      [C] => (_, cont) => cont(KyoSync.asInstanceOf)
+    )
--- src/main/scala/fs2chat/conversion/CETest.scala
+++ src/main/scala/fs2chat/conversion/CETest.scala
@@ -1,11 +1,31 @@
 package fs2chat.conversion
 
+import cats.effect
 import fs2.io.net.Network
-import kyo.{<, Console, Kyo, Sync}
+import kyo.{<, Console, Kyo, KyoApp, Sync}
 
-object TestConv:
-  val allInterfaces = CE[Network](network => network.interfaces.getAll)
+object SyncExample {
+  import cats.effect.{IO, IOApp, Sync}
+  import cats.syntax.all._
 
-  val prg: Unit < (CE[Network] & Sync) = allInterfaces.map: map =>
-    Kyo.foreachDiscard(map.toSeq):
-      case ((k, v)) => Console.printLine(s"$k: $v")
+  def riskyOperation[F[_]: Sync](input: String): F[Int] =
+    Sync[F].delay(input.toInt).handleErrorWith { e =>
+      Sync[F].delay(println(s"Error: ${e.getMessage}")) *> Sync[F].pure(0)
+    }
+
+  def program[F[_]: Sync]: F[Unit] =
+    for {
+      _    <- Sync[F].delay(println("Enter a number:"))
+      line <- Sync[F].delay(scala.io.StdIn.readLine())
+      res  <- riskyOperation(line)
+      _    <- Sync[F].delay(println(s"Result: $res"))
+    } yield ()
+
+}
+
+object TestConv extends KyoApp:
+  def riskyOperation(str: String): Int < CE[effect.Sync] =
+    CE[effect.Sync](SyncExample.riskyOperation(str))
+
+  run:
+    CE.run(riskyOperation("toto"))
